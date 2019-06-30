6cafc62578fa4da2e60f23fc0263c6627577bed1
Richard Arnett <richard@arnett.org>
2019-06-30 04:05:51 -0500

    initial frontend launch

---
+++ build/precache-manifest.50f49ccd42c20affe02db731ccab3599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7477c389814b3805805f1520c0fab03a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1807370cd70df863658f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fa384be827b0fe2dfcb2",
+    "url": "/static/css/main.d35c9be0.chunk.css"
+  },
+  {
+    "revision": "1807370cd70df863658f",
+    "url": "/static/js/2.464e401a.chunk.js"
+  },
+  {
+    "revision": "fa384be827b0fe2dfcb2",
+    "url": "/static/js/main.953ba907.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
